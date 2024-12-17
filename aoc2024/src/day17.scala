162b0bd34c95f40ccf5a919f153ff58f52853ad1
Georg Ofenbeck <georg@ofenbeck.com>
2024-12-17 20:37:50 +0100

    day17 part2

--- aoc2024/src/day17.scala
+++ aoc2024/src/day17.scala
@@ -1,10 +1,11 @@
 import os._
 import scala.collection.{mutable => mu}
 import scala.util.matching.Regex
+import scala.annotation.tailrec
 
 object day17 {
 
-  val test       = true
+  val test       = false
   val printstuff = test
 
   val filePath = {
@@ -17,35 +18,48 @@ object day17 {
 
   val inputStrings: Vector[String] = os.read.lines(filePath).toVector
 
-  case class Registers(a: Int, b: Int, c: Int)
+  case class Registers(a: Long, b: Long, c: Long)
   case class State(program: Vector[Int], regs: Registers, ip: Int, output: List[Int]) {
+
+    def execute(state: State): State = {
+      val operation = state.program(state.ip)
+      operation match
+        case 0 =>
+          adv(state)
+        case 1 =>
+          bxl(state)
+        case 2 =>
+          bst(state)
+        case 3 =>
+          jnz(state)
+        case 4 =>
+          bxc(state)
+        case 5 =>
+          out(state)
+        case 6 =>
+          bdv(state)
+        case 7 =>
+          cdv(state)
+        case _: Int => {
+          assert(false)
+          state
+        }
+    }
+
+    def runStepByStep(): State = {
+      var state = this
+      while (state.ip < state.program.size) {
+        printState(state)
+        println("Press Enter to continue...")
+        scala.io.StdIn.readLine()
+        state = execute(state)
+      }
+      state
+    }
     def run(): State = {
       var state = this
       while (state.ip < state.program.size) {
-        val operation = state.program(state.ip)
-        state = {
-          operation match
-            case 0 =>
-              adv(state)
-            case 1 =>
-              bxl(state)
-            case 2 =>
-              bst(state)
-            case 3 =>
-              jnz(state)
-            case 4 =>
-              bxc(state)
-            case 5 =>
-              out(state)
-            case 6 =>
-              bdv(state)
-            case 7 =>
-              cdv(state)
-            case _: Int => {
-              assert(false)
-              state
-            }
-        }
+        state = execute(state)
       }
       state
     }
@@ -77,14 +91,14 @@ object day17 {
     State(program, regs, 0, List.empty)
   }
 
-  def div(state: State): Int = {
+  def div(state: State): Long = {
     val numerator      = state.regs.a
     val denominatorpow = getOperant(state = state, combo = true)
     var base           = 2
     if (denominatorpow == 0) {
       base = 1
     } else {
-      for (i <- 1 until denominatorpow) {
+      for (i <- 1 until denominatorpow.toInt) {
         base = base << 1
       }
     }
@@ -115,7 +129,7 @@ object day17 {
     } else {
       val operand = getOperant(combo = false, state = state)
       if (operand != state.ip) {
-        return state.copy(ip = operand)
+        return state.copy(ip = operand.toInt)
       } else {
         return state.copy(ip = state.ip + 2)
       }
@@ -133,7 +147,7 @@ object day17 {
   def out(state: State): State = {
     val operand = getOperant(combo = true, state = state)
     val mod     = operand % 8
-    state.copy(ip = state.ip + 2, output = mod +: state.output)
+    state.copy(ip = state.ip + 2, output = mod.toInt +: state.output)
   }
 
   def bdv(state: State): State = {
@@ -144,7 +158,7 @@ object day17 {
     state.copy(ip = state.ip + 2, regs = state.regs.copy(c = div(state)))
   }
 
-  def getOperant(combo: Boolean, state: State): Int = {
+  def getOperant(combo: Boolean, state: State): Long = {
     if (combo == true) {
       val operand = state.program(state.ip + 1)
       operand match
@@ -183,12 +197,11 @@ object day17 {
     val bx = b.run()
     assert(bx.output.reverse == List(0, 1, 2))
 
-
     val d  = State(Vector(1, 7), Registers(0, 29, 0), 0, List.empty)
     val dx = d.run()
     assert(dx.regs.b == 26)
 
-    val f = State(Vector(4, 0), Registers(0, 2024, 43690), 0, List.empty)
+    val f  = State(Vector(4, 0), Registers(0, 2024, 43690), 0, List.empty)
     val fx = f.run()
     assert(fx.regs.b == 44354)
 
@@ -197,26 +210,66 @@ object day17 {
     assert(cx.regs.a == 0)
     assert(cx.output.reverse == List(4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0))
 
-    //println(s" $initalState => ${initalState.run()}")
+    // println(s" $initalState => ${initalState.run()}")
     println(initalState.copy(regs = initalState.regs.copy(a = 117440)).run().output.reverse.mkString(","))
   }
 
-
   def printState(state: State): Unit = {
-    println(s"IP: ${state.ip}")
+    println(s"IP: ${state.ip}; Output: ${state.output}")
     println(s"A: ${state.regs.a.toBinaryString.reverse.padTo(32, '0').reverse} - ${state.regs.a}")
     println(s"B: ${state.regs.b.toBinaryString.reverse.padTo(32, '0').reverse} - ${state.regs.b}")
     println(s"C: ${state.regs.c.toBinaryString.reverse.padTo(32, '0').reverse} - ${state.regs.c}")
-    for(i <- 0 until state.ip){
-        print("  ")
+    for (i <- 0 until state.ip) {
+      print("  ")
     }
     println("+")
     println(state.program.mkString(" "))
   }
 
+  def crack(pos: Int, state: State): Unit = {
+    for (i <- 0 until 8) {
+      var sofar   = state.regs.a
+      val attempt = sofar | i
+      val start   = State(state.program, Registers(attempt, 0, 0), 0, List.empty)
+      val res     = start.run()
+      if (res.output.size == res.program.size) {
+        if (res.program.reverse == res.output) {
+          println(s"Cracked!!! $attempt")
+        }
+      }
+      if (res.output.size > res.program.size) {} else {
+        if (res.output.size > pos) {
+          if (res.output(pos) == state.program.reverse(pos)) {
+            //println(res.output)
+            //println(state.program.reverse.toList)
+            //println(s"$attempt ${attempt << 3} ${attempt.toBinaryString.length()+3}")
+            crack(pos + 1, state.copy(regs = Registers(a = attempt << 3, b = 0, c = 0)))
+
+          }
+        }
+      }
+    }
+  }
+
   @main
   def day17_02(): Unit = {
     val initalState = readInput(inputStrings)
-    printState(initalState)
+   //  initalState.runStepByStep()
+
+    crack(0, initalState.copy(regs = Registers(0, 0, 0)))
+    //println( Integer.MAX_VALUE.toBinaryString.length())
+    /*
+    for(i <- 0 until 8){
+        val start =State(initalState.program,Registers(i,0,0),0,List.empty)
+        val res = start.run()
+//        printState(res)
+      //  println(s"$i -> ${res.output.head}")
+
+    //    val second = start.copy(regs = start.regs.copy(a = i << 3 & i))
+//        printState(second.run())
+
+//        println(s"===================$i")
+    } */
   }
+
 }
